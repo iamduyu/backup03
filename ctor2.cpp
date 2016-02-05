83b5d4e4e110b4e20232a4a471687ec1c76384b3
duyu <13337263177@qq.com>
2016-02-05 09:00:17 -0500

    tutor 8

--- ctor2.cpp
+++ ctor2.cpp
@@ -5,35 +5,62 @@ using namespace std;
 struct Two;
 
 struct One {
- char o;
+ char o='z';
+ 
  One(){
     cout << "One()" << endl;
  }
+ 
+ //init by two
+ /*
  One(Two&){
      o='o';
     cout
-        << "One(Two&)"
+        << "One::One(Two&)"
         << endl;
  }
+ */
+
+ //copy init
  One(const One& o){
      if( o.o == 'o') {
-        this->o= 'p';
+        this->o = 'p';
      }else
-         this->o='o';
+         this->o ='o';
     cout
-        << "One(One&)"
+        << "One::One(One&)"
         << endl;
  }
+
 };
 
 struct Two {
+    // default ctor
+    Two(){
+        cout << "Two()" << endl;
+    }
 
+    // convertor, fron Two to One;
+    
+    operator One() {
+        cout << "Two::One()" << endl;
+    }
+    
 };
 
 int main(void) {
     
-    One o;
     Two t;
+    cout << "-------------pre convention -------------" << endl;
+
+    // 1) call Two::One() convertor
+    // 2) call One::Two() convertor;
+    One o = t;
+ 
+    // if 1) then NO CTOR of one will be called
+    // the new One object is "naked"
+    cout << o.o << endl;
+
     //One o(t);
     // only ctor be called
     //One o = t;
@@ -45,8 +72,12 @@ int main(void) {
 
     // vector test
 
-    vector<One> v {o};
-    cout << v.at(0).o << endl;
+    //vector<One> v(10,One());
+
+    //cout << v.at(0).o << endl;
 
     //v.push_back(o);
+
+    //vector<two> v;
+    
 }
